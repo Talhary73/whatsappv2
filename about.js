e53239045030c9335e27be703f6189a00da2ee19
Muhammad Talha <talhariaz5425869@gmail.com>
2023-11-28 13:15:04 +0000

    updated

--- about.js
+++ about.js
@@ -9,25 +9,20 @@ setInterval(async () => {
   try {
     const { data } = await axios.get('https://tribune.com.pk/weather-updates/islamabad');
     const $ = cheerio.load(data);
-    
     const mainInfo = $('#nav-islamabad > div > div.height-60 > div.date > span').text();
     const currenTemp = $('#tabs-1').text();
-    
     const minutes = Math.floor(i % 60);
     const hours = Math.floor(minutes % 60);
     const seconds = i % 60;
-
     const message = `\nDate: ${mainInfo} \nLocation: Islamabad \nTemp: ${currenTemp.split(' ').join('').split('\n').join('')}`;
-    
-     const showCase = `${hours}h:${minutes}m:${seconds}s` + message
-    
-    // Uncomment the line below if you want to update a profile status
+    const showCase = `${hours}h:${minutes}m:${seconds}s` + message
+     // Uncomment the line below if you want to update a profile status
     await client.updateProfileStatus(showCase);
   
     const jid = '923185853847@s.whatsapp.net'
     const res = await axios.get('https://random.imagecdn.app/1000/1000')
-    fs.writeFileSync('./image.png',res.data)
-    await client.updateProfilePicture(jid, { url: './image.png' })
+    fs.writeFileSync('./image.jpg',res.data)
+    await client.updateProfilePicture(jid, { url: './image.jpg' })
     i = i + 60; // Increment i by 1 second
   } catch (error) {
     const jid = ['923185853847@s.whatsapp.net','923101502365@s.whatsapp.net']
