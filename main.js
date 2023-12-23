58ff527ef0e18faea72f4fa4c1506e244e6f12e2
Muhammad Talha <talhariaz5425869@gmail.com>
2023-12-23 11:00:05 +0000

    updated

--- main.js
+++ main.js
@@ -101,17 +101,6 @@ function getModApkName(inputData, replyNumber) {
 
 const chatGpt = async (client,m,budy)=>{
   
-  console.log(m)
-// mark it unread
-const key = {
-    remoteJid: m.key.remoteJid,
-    id: m.id, // id of the message you want to read
-    participant: m.sender // the ID of the user that sent the  message (undefined for individual chats)
-}
-// pass to readMessages function
-// can pass multiple keys to read multiple messages as well
-await client.readMessages([key])    
-        
 
 
   const apiKeys = [process.env.OPENAI_API_KEY,process.env.API_KEY_1,process.env.API_KEY_3]
@@ -657,6 +646,547 @@ await client.readMessages([key])
 
       
 } 
+
+const bardTools = async (client, m, budy) => {
+  console.log(m.sender);
+  const API_KEY = "AIzaSyAz4YCKV7YhqLaw0oZW-xQZrKGC91xacqc"; // Replace with your actual API key
+  let data = [];
+
+  if (!fs.existsSync(`./data/${m.sender.split("@")[0]}.json`)) {
+    fs.writeFileSync(
+      `./data/${m.sender.split("@")[0]}.json`,
+      JSON.stringify([]),
+    );
+  } else {
+    data = fs.readFileSync(`./data/${m.sender.split("@")[0]}.json`, {
+      encoding: "utf-8",
+    });
+  }
+  data = JSON.parse(data);
+  const user = {
+    role: "user",
+    parts: [
+      {
+        text: budy,
+      },
+    ],
+  };
+  // console.log([...data, user]);
+
+  // return;
+
+  const tools = [
+    {
+      name: "gimage",
+      description: "This will send User picture according to giving text.",
+      parameters: {
+        type: "object",
+        properties: {
+          address: {
+            type: "string",
+
+            description: "Text for function so it will send picture",
+          },
+          number: {
+            type: "number",
+
+            description: "number of how many pictures to send default is 3",
+          },
+          width: {
+            type: "number",
+
+            description: "minimum width for picture",
+          },
+          height: {
+            type: "number",
+
+            description: "minimum height for picture",
+          },
+        },
+        required: ["address", "number", "width", "height"],
+      },
+    },
+    {
+      name: "getYtAudio",
+      description: "send youtube video.",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "Title of video",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "spotify_dl",
+      description: "Downloads spotify songs",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "Link",
+          },
+          type: {
+            type: "string",
+            enum: ["single", "playlist", "album"],
+            description: "Type of link.",
+          },
+        },
+        required: ["text", "type"],
+      },
+    },
+    {
+      name: "tiktok_dl",
+      description: "Download Tiktok Video..",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "Direct link of Tiktok of video",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "doc",
+      description:
+        "Send Documents based on Text from wikipedia. Also good for straight definitions.",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "This is the title for searching document",
+          },
+        },
+        required: ["text", "pdf"],
+      },
+    },
+    {
+      name: "instaDl",
+      description: "download instagram video or image and send it to user",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "This is direct link of instagram post",
+          },
+          thumbnail: {
+            type: "boolean",
+
+            description: "This is thumbnail for video",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "gptUrl",
+      description:
+        "Url and question as input send it to AI chatBot and respond back required result",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "question of User that best describe the user request",
+          },
+          url: {
+            type: "string",
+
+            description: "Direct Url that best describe the user request",
+          },
+        },
+        required: ["text", "url"],
+      },
+    },
+    {
+      name: "fb",
+      description: "It will download and send facebook video",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "This is a Direct Url.",
+          },
+        },
+        required: ["text", "hd", "sd"],
+      },
+    },
+    {
+      name: "sendFile",
+      description: "Send any type of File.",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "This is a Direct Url.",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "getYtvieo",
+      description: "Download a youtube Video",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "This is a Direct link of youtube video.",
+          },
+          itag: {
+            type: "number",
+
+            description: "Itag for video",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "google",
+      description: "This will google anything and send urls to user.",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description:
+              "This will return Title for google search that best describe the user request",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "audioYt",
+      description: "This downloads audio and sends it.",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: " Direct link of Youtube video.",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "ytaFromText",
+      description: "Send audio songs and audio form of youtube video",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: " Title of audio ",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "yts",
+      description: "send the links of youtube videos based on Text",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: " Title of videos",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "ttsv2",
+      description:
+        "This will take text as input and convert into audio and send to the user",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description:
+              "This will return text of function that needs to be converted to speech.",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "ssv2",
+      description: "This function will send Screenshot to User.",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description:
+              "This is a url for webpage. It should start with https://",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "image",
+      description: "It will send Image from Text",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "This is text",
+          },
+          width: { type: "string", description: "Width of Image" },
+          height: { type: "string", description: "height of Image" },
+          fontSize: { type: "string", description: "font size of Image" },
+          backgoundColor: {
+            type: "string",
+            description: "font color of Image",
+          },
+        },
+        required: ["text", "width", "height", "fontSize", "backgoundColor"],
+      },
+    },
+    {
+      name: "sticker",
+      description: "create sticker from text.",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "simple text.",
+          },
+          height: { type: "string", description: "Height of sticker" },
+          width: { type: "string", description: "width of sticker" },
+          fontFamily: { type: "string", description: "Font Family of Text" },
+          fontColor: { type: "string", description: "Font Color of Text" },
+          fontSize: { type: "string", description: "Font size of Text" },
+          align: { type: "string", description: "align" },
+          valign: { type: "string", description: "valign" },
+          borderColor: { type: "string", description: "border color" },
+          backgoundColor: {
+            type: "string",
+            description: "Background color code",
+          },
+          underLineColor: {
+            type: "string",
+            description: "UnderLine color code",
+          },
+          marginTop: { type: "string", description: "Margin top" },
+          marginBottom: { type: "string", description: "Margin bottom" },
+          underlineSize: { type: "string", description: "under line size" },
+        },
+        required: [
+          "text",
+          "width",
+          "height",
+          "fontFamily",
+          "fontColor",
+          "fontSize",
+          "align",
+          "valign",
+          "borderColor",
+          "backgroundColor",
+          "underLineColor",
+          "marginTop",
+          "marginBottom",
+          "underlineSize",
+        ],
+      },
+    },
+    {
+      name: "bardGemini",
+      description:
+        "This will send the request to google gemini. chat bot. It will answer more difficult question.",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "string",
+
+            description: "the request.",
+          },
+        },
+        required: ["text"],
+      },
+    },
+    {
+      name: "clear",
+      description: "This will clear previous response with chatBot",
+      parameters: {
+        type: "object",
+        properties: {
+          text: {
+            type: "boolean",
+
+            description: "This will tell weather clear data or not.",
+          },
+        },
+        required: ["text"],
+      },
+    },
+  ];
+  const data1 = {
+    contents: [...data, user], // Include the new user data in the contents array
+    tools: [
+      {
+        functionDeclarations: tools,
+      },
+    ],
+  };
+
+  try {
+    const response = await axios.post(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`,
+      data1,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    const msg = response.data.candidates[0].content.parts[0];
+
+    console.log(response.data.candidates[0].content.parts[0]);
+    if (
+      response.data.candidates[0].content.parts[0].functionCall?.args
+        .extension == "default_api"
+    ) {
+      bardTools(client, m, budy);
+      return;
+    }
+    const newData = {
+      role: response.data.candidates[0].content.role,
+      parts: response.data.candidates[0].content.parts,
+    };
+
+    data.push(user);
+    data.push(newData); // Push the new data to the history array
+    fs.writeFileSync(
+      `./data/${m.sender.split("@")[0]}.json`,
+      JSON.stringify(data),
+    ); // Write the updated history to the file
+
+    if (msg.text) {
+      client.sendMessage(m.chat, { text: msg.text }, { quoted: m });
+    } else if (msg.functionCall) {
+       const res = msg.functionCall;
+      const arg = msg.functionCall.args;
+      if (res.name == "gimage") {
+        gimage(client, m, arg.address, arg.number, arg.width, arg.height);
+      } else if (res.name == "getYtAudio") {
+        getYtAudio(client, m, arg.text);
+      } else if (res.name == "ttsv2") {
+        ttsv3(client, m, arg.text);
+      } else if (res.name == "ytaFromText") {
+        ytaFromText(client, m.sender, arg.text);
+      } else if (res.name == "clear") {
+        fs.unlinkSync(`./user/${m.sender.split("@")[0]}.json`);
+        client.sendMessage(m.sender, { text: "Cleared old data" });
+        console.log("running clear");
+        return;
+      } else if (res.name == "yts") {
+        yts(client, m.sender, arg.text);
+      } else if (res.name == "getYtvieo") {
+        ytNew(client, m, arg.text, arg.itag);
+      } else if (res.name == "audioYt") {
+        audioYt(client, m, arg.text);
+      } else if (res.name == "sendFile") {
+        sendFile(client, m, arg.text, "./assets");
+      } else if (res.name == "google") {
+        google(client, m.sender, arg.text);
+      } else if (res.name == "gptUrl") {
+        gptUrl(client, m, arg.text, arg.url);
+      } else if (res.name == "instaDl") {
+        instaDl(client, m, arg.text, arg.thumbnail);
+      } else if (res.name == "ssv2") {
+        ssv2(client, m, arg.text);
+      } else if (res.name == "google") {
+        google(client, m.sender, arg.text);
+      } else if (res.name == "doc") {
+        wiki1(client, m, arg.text, true);
+      } else if (res.name == "tiktok_dl") {
+        tiktok_dl(client, m, arg.text);
+      } else if (res.name == "spotify_dl") {
+        spotify_dl(client, m, arg.text, arg.type);
+      } else if (res.name == "fb") {
+        fb(client, m, arg.text);
+      } else if (res.name == "sticker") {
+        const data = {
+          width: arg.width,
+          height: arg.height,
+          fontFamily: arg.fontFamily,
+          fontColor: arg.fontColor,
+          fontSize: arg.fontSize,
+          align: arg.align,
+          valign: arg.valign,
+          borderColor: arg.borderColor,
+          backgoundColor: arg.backgoundColor,
+          underLineColor: arg.underLineColor,
+          marginBottom: arg.marginBottom,
+          marginTop: arg.marginTop,
+          underlineSize: arg.underlineSize,
+        };
+        sticker(client, m, arg.text, data);
+      } else if (res.name == "image") {
+        image(
+          client,
+          m,
+          arg.text,
+          arg.width,
+          arg.height,
+          arg.fontSize,
+          arg.backgoundColor,
+        );
+      } else if (res.name == "bardGemini") {
+        bard(client, m, budy);
+      }
+
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  // Call the function
+};
+// bardTools("", { sender: "34234234@gmai.com" }, "can you send images?");
+
+
 
 module.exports = sansekai = async (client, m, chatUpdate, store) => {
  
@@ -1261,7 +1791,7 @@ To get started, just type one of these commands, and I'll help you out! 🚀
            await sendFile(client,m,text,'./assets')
         } 
         else if(command == 'bot'){
-                 client.sendMessage(id,{text:'*|BOT_SELECTOR|*\n\nPlease reply to one of these *number*. \n\n 1:gpt \n\n 2:bard \n\n 3:only-gpt'})
+                 client.sendMessage(id,{text:'*|BOT_SELECTOR|*\n\nPlease reply to one of these *number*. \n\n 1:gpt \n\n 2:bard \n\n 3:only-gpt \n\n 4:bard-only'})
 
         }
         else {
@@ -1278,8 +1808,9 @@ To get started, just type one of these commands, and I'll help you out! 🚀
              chatGpt(client,m,budy)
              
            }
-             else if(data.filter(el=>el.id === m.sender.split('@')[0])[0].bot === 'bard') bard(client,m, budy)
-            else gpt(client,m,budy)
+             else if(data.filter(el=>el.id === m.sender.split('@')[0])[0].bot === 'bard') bardTools(client,m, budy)
+            else if(data.filter(el=>el.id === m.sender.split('@')[0])[0].bot === 'bard-only')  bard(client,m,budy)
+          else gpt(client,m,budy)
         }
 
       } catch (err) {
