f1bc60f3a24f5597ace905fbb2f9a4bfb2981899
Muhammad Talha <talhariaz5425869@gmail.com>
2024-02-03 12:19:56 +0000

    dsf

--- ws.js
+++ ws.js
@@ -1,6 +1,13 @@
-const { Tiktok } = require('@xct007/tiktok-scraper');
+const  { music } = require("@xct007/frieren-scraper");
 
-const url = 'https://vt.tiktok.com/ZGJBtcsDq/';
-Tiktok(url).then((json) => {
-  console.log(json['download'].nowm);
-});
+// search apps/games
+const APKMODY = async()=>{
+// more short query, more results found.
+const ArrObj = await music.search("pakistan zindabad");
+console.log(ArrObj);
+// fetch direct download url
+// High memory usage. LOL
+// const Obj = await apkmody.download("https://APKMODY_URL");
+// console.log(Obj);
+}
+APKMODY()
