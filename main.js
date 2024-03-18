f949ccf90a8efffd0b8deb4e05aaaeeb93e30ead
Muhammad Talha <talhariaz5425869@gmail.com>
2024-03-18 05:52:04 +0500

    Update main.js

--- main.js
+++ main.js
@@ -887,7 +887,7 @@ const id = m.sender;
  if(!User?.[0]){
   await UserModel.create({name:await client.getName(m.sender), id:m.sender, bot:'gpt-4',audio:true})
   User.push({name:await client.getName(m.sender), id:m.sender, bot:'gpt-4',audio:true})
-  client.sendMessage(id,{text:'*|BOT_SELECTOR|*\n\nPlease reply to one of these *number*. \n\n 1:gpt \n\n 2:bard \n\n 4:bard-only \n\n 5:gpt-4'})
+  client.sendMessage(id,{text:'*|BOT_SELECTOR|*\n\nPlease reply to one of these *number*. \n\n 2:bard \n\n 4:bard-only \n\n 5:gpt-4'})
   client.sendMessage(id,{text:'Send /audio To Turn off/on sending Audio'})
 }
 //  console.log(m.chat)
