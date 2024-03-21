f51af485ed1e9fe6d629cea0b19f0a5b33ec35ca
Muhammad Talha <talhariaz5425869@gmail.com>
2024-03-21 14:13:46 +0000

    updated

--- testnew.js
+++ testnew.js
@@ -1,13 +1,29 @@
-const axios = require('axios')
-const fun = async ()=>{
-    try {
-         const res = await axios.get('https://random.imagecdn.app/1000/1000',{
-       responseType: 'arraybuffer'
-    })
-   
-   
-    } catch (error) {
-        console.log(error)
-    }
+let i = 0;
+// const wait = () => {
+//   if (i == 1) {
+//     i = i + 1;
+//     return "Wait a moment, I'm thinking.";
+//   }
+
+//   if (i % 2 == 0) {
+//     i = i + 1;
+//     return "Wait a moment, I'm thinking..";
+//   } else if (i % 3 == 0) {
+//     i = i + 1;
+//     return "Wait a moment, I'm thinking...";
+//   } else if (i % 4 == 0) {
+//     i = i + 1;
+//     return "Wait a moment, I'm thinking....";
+//   }
+// };
+const wait = () => {
+  i = i + 1;
+  let j = i % 4;
+  if (j == 0) return "Wait a moment, I'm thinking.";
+  else if (j == 1) return "Wait a moment, I'm thinking..";
+  if (j == 2) return "Wait a moment, I'm thinking...";
+  if (j == 3) return "Wait a moment, I'm thinking....";
+};
+for (let j = 0; j <= 100; j++) {
+  console.log(wait(j));
 }
-fun()
