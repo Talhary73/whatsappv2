e89e9407845d5cc7ac7e621bb2e42d5781dfb80c
Muhammad Talha <talhariaz5425869@gmail.com>
2024-04-05 11:14:19 +0000

    sdf

--- testnew.js
+++ testnew.js
@@ -1,29 +1,38 @@
-let i = 0;
-// const wait = () => {
-//   if (i == 1) {
-//     i = i + 1;
-//     return "Wait a moment, I'm thinking.";
-//   }
-
-//   if (i % 2 == 0) {
-//     i = i + 1;
-//     return "Wait a moment, I'm thinking..";
-//   } else if (i % 3 == 0) {
-//     i = i + 1;
-//     return "Wait a moment, I'm thinking...";
-//   } else if (i % 4 == 0) {
-//     i = i + 1;
-//     return "Wait a moment, I'm thinking....";
-//   }
-// };
-const wait = () => {
-  i = i + 1;
-  let j = i % 4;
-  if (j == 0) return "Wait a moment, I'm thinking.";
-  else if (j == 1) return "Wait a moment, I'm thinking..";
-  if (j == 2) return "Wait a moment, I'm thinking...";
-  if (j == 3) return "Wait a moment, I'm thinking....";
+const axios = require("axios");
+const activate = async () => {
+  try {
+    const res = await axios.post(
+      "https://faucetearner.org/api.php?act=faucet",
+      {},
+      {
+        headers: {
+          Host: "faucetearner.org",
+          "Content-Length": "2",
+          "Sec-Ch-Ua":
+            '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+          Accept: "application/json, text/javascript, */*; q=0.01",
+          "Content-Type": "application/json",
+          "X-Requested-With": "XMLHttpRequest",
+          "Sec-Ch-Ua-Mobile": "?1",
+          "User-Agent":
+            "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36",
+          "Sec-Ch-Ua-Platform": '"Android"',
+          Origin: "https://faucetearner.org",
+          "Sec-Fetch-Site": "same-origin",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Dest": "empty",
+          Referer: "https://faucetearner.org/faucet.php",
+          "Accept-Encoding": "gzip, deflate, br, zstd",
+          "Accept-Language": "en-US,en;q=0.9",
+          Cookie:
+            "pid=757772534530; googtrans=/en/en; reg=1; login=1; user=894550846617-45.55.40.168; show_nt1=1; Hm_lvt_2b147ccaeef7e49f5f9553cadfdf8428=1712299490; Hm_lpvt_2b147ccaeef7e49f5f9553cadfdf8428=1712299637",
+        },
+      }
+    );
+    console.log(res.data);
+  } catch (error) {
+    console.log(error);
+  }
 };
-for (let j = 0; j <= 100; j++) {
-  console.log(wait(j));
-}
+module.exports = activate;
+activate();
